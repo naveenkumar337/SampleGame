cc087d0a096acf640a77957e0b69de87aad3ddd5
naveen v <naveenajay337@gmail.com>
2020-05-02 22:03:24 +0530

    second push

--- SampleGame/SampleGamePage.js
+++ SampleGame/SampleGamePage.js
@@ -18,8 +18,6 @@ function shuffle(arr) {
 function randomNumber(start, end) {
     return Math.floor(Math.random() * (end - start));
 }
-
-
 const vartable = document.querySelector('.divtable');
 console.log(vartable);
 var html = "";
@@ -30,19 +28,48 @@ function getHTML(data,key) {
     for (let i = 0; i < data.length; i++) {
         debugger;
         if (i === 0) {
-            html += `<tr><td class='cls${key}'>${data[i]}</td>`;
+            html += `<tr><td class='cls${key}'><button class='btn${i}'>${data[i]}</button></td>`;
         }
         else if (i % 10 === 0) {
-            html += `</tr><tr><td class='cls${key}'>${data[i]}</td>`;
+            html += `</tr><tr><td class='cls${key}'><button class='btn${i}'>${data[i]}</button></td>`;
         }
         else if (i === data.length - 1) {
-            html += `<td class='cls${key}'>${data[i]}</td ></tr>`;
+            html += `<td class='cls${key}'><button class='btn${i}'>${data[i]}</button></td ></tr>`;
         }
         else {
-            html += `<td class='cls${key}'>${data[i]}</td>`;
+            html += `<td class='cls${key}'><button class='btn${i}'>${data[i]}</button></td>`;
         }
     }
     return html;
 }
 console.log(html);
-vartable.innerHTML=html;
+vartable.innerHTML = html;
+
+var root = document.querySelectorAll('.divtable tr td');
+var rootnode = shuffle(Object.values(root).map(function (key) { return key }));
+var i = 0; root.forEach(function (y) { y.outerHTML = rootnode[i].outerHTML; i++; });
+
+var buttonnodes = document.querySelectorAll('.divtable tr td button');
+for (let m = 0; m < buttonnodes.length; m++) {
+    buttonnodes[m].addEventListener('click', buttonClick);
+}
+var a1 = 0,b1=0;
+function buttonClick(clicked) {
+    if (clicked.path[1].className === 'clsA')
+    {
+        if (parseInt(clicked.path[1].innerText) === a1+1) {
+            var parentnode = clicked.path[0].parentNode;
+            parentnode.style.backgroundColor = "White";
+            a1 = parseInt(clicked.path[1].innerText);
+        }
+
+        //document.querySelector(`.${clicked.path[0]}`).parentNode.style.color = 'green'; 
+    } else if (clicked.path[1].className === 'clsB') {
+        if (parseInt(clicked.path[1].innerText) === b1 + 1) {
+            var parentnode = clicked.path[0].parentNode;
+            parentnode.style.backgroundColor = "green";
+            b1 = parseInt(clicked.path[1].innerText);
+        }
+    } else { }
+    console.log(clicked.path[1].innerText);
+}
